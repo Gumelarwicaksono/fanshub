475327803049dac28efaf8603b189ebc7132fd0f
gumelar <gumelar.wicaksono6@gmail.com>
2024-10-14 22:15:20 +0700

    add web js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,33 @@
+const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+const axios = require('axios');
 var express = require('express');
-var router = express.Router();
 const { v4: uuidv4 } = require('uuid');
 const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
+var router = express.Router();
 const { auth, authorize, createUserWithEmailAndPassword, db, dbsRef, deleteObject, get, getDownloadURL, push, ref, remove, set, signInWithEmailAndPassword, storage, update, uploadBytes } = require('./../firebase');
+const { status } = require('express/lib/response');
+const client = new Client({
+  puppeteer: {
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  },
+  authStrategy: new LocalAuth(),
+});
+client.on('qr', (qr) => {
+  qrcode.generate(qr, { small: true });
+});
 
+client.on('ready', () => {
+  console.log('Client is ready!');
+});
 const upload = multer({ storage: multer.memoryStorage() });
 
+function sanitizeFilename(filename) {
+  // Hapus karakter yang tidak valid (ganti dengan '_')
+  return filename.replace(/[^a-z0-9\.\-_ ]/gi, '_');
+}
 function register0(email, password, displayName) {}
 /* GET home page. */
 // router.get('/', function (req, res, next) {
@@ -43,7 +65,7 @@ router.get('/', async (req, res) => {
       baner.id = childSnapshot.key;
       baners.push(baner);
     });
-
+    console.log(products);
     res.render('index', {
       products: products,
       categories: categories,
@@ -111,27 +133,27 @@ router.post('/products', authorize, upload.single('image'), async function (req,
       await push(categoryRef, { name: categoryName });
     }
     // Upload gambar ke Firebase Storage (jika ada)
-    // let imageUrl = '';
-    // if (file) {
-    //   const imageRef = dbsRef(storage, `images/${uuidv4()}-${file.originalname}`);
-    //   await uploadBytes(imageRef, file.buffer);
-    //   imageUrl = await getDownloadURL(imageRef);
-    // }
-
     let imageUrl = '';
     if (file) {
       const imageRef = dbsRef(storage, `images/${uuidv4()}-${file.originalname}`);
-      const metadata = {
-        contentType: file.mimetype, // e.g. image/jpeg, image/png, etc.
-        customMetadata: {
-          description: 'This is a products image',
-        },
-        contentDisposition: 'inline; filename="' + file.originalname + '"',
-        cacheControl: 'public, max-age=31536000', // cache for 1 year
-      };
-      await uploadBytes(imageRef, file.buffer, metadata);
+      await uploadBytes(imageRef, file.buffer);
       imageUrl = await getDownloadURL(imageRef);
     }
+
+    // let imageUrl = '';
+    // if (file) {
+    //   const imageRef = dbsRef(storage, `images/${uuidv4()}-${file.originalname}`);
+    //   const metadata = {
+    //     contentType: file.mimetype, // e.g. image/jpeg, image/png, etc.
+    //     customMetadata: {
+    //       description: 'This is a products image',
+    //     },
+    //     contentDisposition: 'inline; filename="' + file.originalname + '"',
+    //     cacheControl: 'public, max-age=31536000', // cache for 1 year
+    //   };
+    //   await uploadBytes(imageRef, file.buffer, metadata);
+    //   imageUrl = await getDownloadURL(imageRef);
+    // }
     // Simpan produk ke Realtime Database
     const newProductRef = push(ref(db, 'products'));
     await set(newProductRef, {
@@ -146,7 +168,6 @@ router.post('/products', authorize, upload.single('image'), async function (req,
       terjual,
       gudang,
     });
-
     res.status(201).json('success add product sablonika ');
   } catch (error) {
     console.error(error);
@@ -349,4 +370,170 @@ router.delete('/products/:id/delete', authorize, async (req, res) => {
     res.status(500).send('Terjadi kesalahan.');
   }
 });
+
+function formatAngka(angka) {
+  return angka.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+}
+
+// router.post('/share-whatsapp', async (req, res) => {
+//   const { imageUrl, name, price, subPrice, discount, detailUrl } = req.body;
+//   const recipientPhoneNumber = '+6283166383802';
+//   const hemat = price - subPrice;
+
+//   try {
+//     // Mengunduh gambar dari Firebase Storage
+//     let media;
+
+//     // Jika imageUrl adalah gambar:
+//     if (imageUrl.match(/\.(jpeg|jpg|png|gif)$/i)) {
+//       // Validasi ekstensi gambar
+//       const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+//       const imageBuffer = Buffer.from(response.data, 'binary');
+//       media = new MessageMedia('image/jpeg', imageBuffer, 'image.jpg'); // Ganti mimetype jika perlu
+//     } else {
+//       // Jika imageUrl adalah video/GIF, coba kirim sebagai URL (pastikan formatnya didukung):
+//       media = await MessageMedia.fromUrl(imageUrl);
+//     }
+//     // Mengirim gambar ke WhatsApp dengan caption
+//     const caption = `
+// 🔥 PROMO TERBATAS! ${name} 🔥
+
+// ✨ ${name} ✨
+
+// Harga Normal: Rp${formatAngka(price)}
+// Diskon: ${discount}%
+// Harga Spesial kaos ${name} setelah diskon: *Rp${formatAngka(subPrice)}*
+// Anda Hemat: *Rp${formatAngka(hemat)}*
+
+// Tampil stylish dan percaya diri dengan produk terbaru kami! Bahan katun premium yang nyaman dan desain grafiti yang edgy bikin kamu jadi pusat perhatian.
+// Stok Terbatas! Pesan Sekarang Sebelum Kehabisan!
+
+// ➡️ Klik di sini untuk cek ongkir dan pesan dari website: ${detailUrl}`;
+
+//     await client.sendMessage(recipientPhoneNumber, media, { caption: caption });
+
+//     // Redirect ke WhatsApp
+//     res.redirect(`https://wa.me/${recipientPhoneNumber}?text=➡️ Silahkan Tanya Harga Dengan ONGKIR khusus jakarta barat free ongkir`);
+//   } catch (error) {
+//     console.error('Error downloading or sending image:', error);
+//     res.status(500).send('Error processing request');
+//   }
+// });
+
+router.post('/share-whatsapp', async (req, res) => {
+  try {
+    const imageUrl = req.body.imageUrl; // Ambil imageUrl dari body request
+
+    if (!imageUrl) {
+      return res.status(400).json({
+        status: 'error',
+        message: 'imageUrl is required',
+      });
+    }
+
+    const parsedUrl = new URL(imageUrl); // Parsing URL
+    const fileName = parsedUrl.pathname.split('/').pop(); // Ambil nama file dari path
+    // const fileExtension = path.extname(fileName);
+    const uniqueFileName = `${sanitizeFilename(fileName)}`;
+    const filePath = `./wafoto/${uniqueFileName}`;
+
+    const response = await axios({
+      url: imageUrl,
+      method: 'GET',
+      responseType: 'arraybuffer',
+    });
+
+    fs.writeFileSync(filePath, response.data);
+
+    console.log('Gambar berhasil disimpan ke:', filePath);
+
+    // Send WhatsApp message with caption
+    const recipientPhoneNumber = '+6283166383802';
+
+    const caption = `
+    🔥 PROMO TERBATAS! ${req.body.name} 🔥
+
+    ✨ ${req.body.name} ✨
+
+    Harga Normal: Rp${formatAngka(req.body.price)}
+    Diskon: ${req.body.discount}%
+    Harga Spesial kaos ${req.body.name} setelah diskon: *Rp${formatAngka(req.body.subPrice)}*
+    Anda Hemat: *Rp${formatAngka(req.body.price - req.body.subPrice)}*
+
+    Tampil stylish dan percaya diri dengan produk terbaru kami! Bahan katun premium yang nyaman dan desain grafiti yang edgy bikin kamu jadi pusat perhatian. 
+    Stok Terbatas! Pesan Sekarang Sebelum Kehabisan!
+
+    ➡️ Klik di sini untuk cek ongkir dan pesan dari website: ${req.body.detailUrl}`;
+
+    client.on('message', async (msg) => {
+      if (msg.body === '!send-media') {
+        const media = MessageMedia.fromFilePath(filePath);
+
+        await client.sendMessage(recipientPhoneNumber, media, { caption: caption });
+      }
+    });
+
+    // Hapus gambar di direktori wafoto
+    fs.unlinkSync(filePath);
+    console.log('Gambar berhasil dihapus dari direktori wafoto');
+
+    // Redirect ke WhatsApp
+    res.redirect(`https://wa.me/${recipientPhoneNumber}?text=➡️ Silahkan Tanya Harga Dengan ONGKIR khusus jakarta barat free ongkir`);
+  } catch (error) {
+    console.error('Error menyimpan gambar atau mengirim ke WhatsApp:', error);
+    res.status(500).json({
+      status: 'error',
+      message: 'Terjadi kesalahan saat menyimpan gambar atau mengirim ke WhatsApp',
+      error: error.message, // Optional: kirimkan pesan error untuk debugging
+    });
+  }
+});
+
+router.post('/simpanfoto', async (req, res) => {
+  try {
+    const imageUrl = 'https://firebasestorage.googleapis.com/v0/b/bioproduct-fa5a4.appspot.com/o/images%2Fa421abc5-526f-48ec-90b4-b788275f662e-prod1.png?alt=media&token=84eaaec8-0748-4a21-a534-f132b7b0b645'; // Ambil imageUrl dari body request
+
+    if (!fs.existsSync('./wafoto')) {
+      fs.mkdirSync('./wafoto');
+    }
+    if (!imageUrl) {
+      return res.status(400).json({
+        status: 'error',
+        message: 'imageUrl is required',
+      });
+    }
+
+    const parsedUrl = new URL(imageUrl); // Parsing URL
+    const fileName = parsedUrl.pathname.split('/').pop(); // Ambil nama file dari path
+    const fileExtension = path.extname(fileName);
+    const uniqueFileName = `${uuidv4()}-${sanitizeFilename(fileName)}`;
+    const filePath = `./wafoto/${uniqueFileName}${fileExtension}`;
+
+    const response = await axios({
+      url: imageUrl,
+      method: 'GET',
+      responseType: 'arraybuffer',
+    });
+
+    fs.writeFileSync(filePath, response.data);
+
+    console.log('Gambar berhasil disimpan ke:', filePath);
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Gambar berhasil disimpan ke wafoto',
+      filePath: filePath,
+    });
+  } catch (error) {
+    console.error('Error menyimpan gambar:', error);
+    res.status(500).json({
+      status: 'error',
+      message: 'Terjadi kesalahan saat menyimpan gambar',
+      error: error.message, // Optional: kirimkan pesan error untuk debugging
+    });
+  }
+});
+
+client.initialize();
+
 module.exports = router;
